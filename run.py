dadba2deafabb4d8c5a978d8da23215f5728b554
overfero <fehruputramen22@gmail.com>
2024-05-26 20:57:51 +0700

    time to deploy

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
 import gradio as gr
+from fastapi import FastAPI
 
 from gradio_ui import demo
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return 'Gradio app is running at /gradio', 200
 
-app = gr.mount_gradio_app(app, demo, path='/aibeecara')
+@app.get("/")
+async def root():
+    return "Aibeecara app is running at /aibeecara", 200
+
+
+app = gr.mount_gradio_app(app, demo, path="/aibeecara")
